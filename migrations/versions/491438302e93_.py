08adb1394b8f8e362fc0f6a9d6262096fb2e1352
Kamal Koranga <kamalkornga75@gmail.com>
2022-09-19 14:11:44 +0530

    some bugs

---
+++ migrations/versions/491438302e93_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 491438302e93
+Revises: 
+Create Date: 2022-09-19 09:52:47.809785
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '491438302e93'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
