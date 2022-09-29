5d9d8329cf80170ea1a92c2de30d92c67dea0006
Kamal Koranga <kamalkornga75@gmail.com>
2022-09-29 19:43:57 +0530

    heroku deployment changes

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://codehubv0_1_user:vJYFkroV6xLWocjgxKW7MrQ4t9RsZpzs@dpg-cci6ulpa6gdiindmmaug-a.singapore-postgres.render.com/codehubv0_1'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://codehubv0_1_user:vJYFkroV6xLWocjgxKW7MrQ4t9RsZpzs@dpg-cci6ulpa6gdiindmmaug-a.singapore-postgres.render.com/codehubv0_1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_ENABLE_POOL_PRE_PING = True
     POSTS_PER_PAGE = 30
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
