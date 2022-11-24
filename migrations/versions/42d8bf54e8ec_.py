e33f0f3242b2cb194b8d8d1b670e3b9eb57fe318
Kamal Koranga <kamalkornga75@gmail.com>
2022-11-24 23:48:11 +0530

    migration

---
+++ migrations/versions/42d8bf54e8ec_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 42d8bf54e8ec
+Revises: a3ec56a57813
+Create Date: 2022-11-24 23:47:28.178899
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '42d8bf54e8ec'
+down_revision = 'a3ec56a57813'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
