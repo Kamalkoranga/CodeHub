3d5f6719ddb356347d9f53828def9762ee8e1a64
Kamal Koranga <kamalkornga75@gmail.com>
2022-12-01 01:36:44 +0530

    some tweaks

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -24,7 +24,7 @@ flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email",
             "openid"],
-    redirect_uri="https://finalyearproject-production.up.railway.app/auth/callback"
+    redirect_uri="https://codehub.gq/auth/callback"
 )
 '''
 @bp.route('/login', methods=['GET', 'POST'])
