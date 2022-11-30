8dc3d143729907a93243f99515879ea3264bcf00
Kamal Koranga <kamalkornga75@gmail.com>
2022-12-01 00:51:14 +0530

    some tweaks

--- codehub.py
+++ codehub.py
@@ -6,3 +6,6 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Upload': Upload, 'comment': Comment}
+
+if __name__ == '__main__':
+    app.run(ssl_context="adhoc")
