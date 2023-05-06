61fdae0a8186e9b42f9c75ac3e405922591b7ac9
Kamal Koranga <kamalkoranga75@gmail.com>
2023-05-06 16:18:29 +0530

    setup at render with docker + sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
     login_manager.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
