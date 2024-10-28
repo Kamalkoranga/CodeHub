a8a9e6520725bcb92152b2709ad94abbf0a310cd
Kamal Koranga <kamalkoranga75@gmail.com>
2024-10-28 22:29:56 +0530

    updated information

--- codehub.py
+++ codehub.py
@@ -1,7 +1,12 @@
+from flask import jsonify
 from app import create_app, db
 from app.models import Upload, User, Comment
 
 app = create_app()
+
+@app.route('/system_status', methods=['GET'])
+def system_status():
+    return jsonify({'message': "System is running properly ✅"}), 200
 
 
 @app.shell_context_processor
